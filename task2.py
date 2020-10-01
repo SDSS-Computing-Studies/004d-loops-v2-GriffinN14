2d66e90a9e12c733ac9f9f33bf64b630e16bf786
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-10-01 12:48:22 -0700

    Update task2.py

---
+++ task2.py
@@ -0,0 +1,6 @@
+name = input("Enter a name ")
+names = ("Griffin","Daniel","Jimmy","Ian","Samantha","Julia","Tom","Jack")
+for n in range(8):
+    if name == names[n]:
+        print("Match")
+        break
