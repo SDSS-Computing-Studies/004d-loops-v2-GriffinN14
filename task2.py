3b9aa0330fd160943ed5a8a7dbf8ca107ad44a31
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-11-13 12:48:45 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,6 +1,10 @@
 name = input("Enter a name ")
 names = ("Griffin","Daniel","Jimmy","Ian","Samantha","Julia","Tom","Jack")
+found = 0
 for n in range(8):
     if name == names[n]:
         print("Match")
+        found = 1
         break
+if found == 0:
+    print ("That name is on the list")
