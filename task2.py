f79ee3ab8e6a29e831f05ad723f6e17b4efe26cf
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-11-13 12:50:07 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -3,8 +3,8 @@ names = ("Griffin","Daniel","Jimmy","Ian","Samantha","Julia","Tom","Jack")
 found = 0
 for n in range(8):
     if name == names[n]:
-        print("Match")
+        print("That name is on the list")
         found = 1
         break
 if found == 0:
-    print ("That name is on the list")
+    print ("That name is not on the list")
